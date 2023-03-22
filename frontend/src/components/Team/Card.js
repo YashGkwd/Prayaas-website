cff8e2b2fe2a61e6969a515e4b226742a5afe967
Yash Gaikwad <gaikwadyash412@gmail.com>
2023-03-23 02:01:08 +0530

    added cards

---
+++ frontend/src/components/Team/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Team.css"
+function Card() {
+  return (
+    <>
+      <div className="card" >
+  <img src="https://i.stack.imgur.com/5Kgaq.jpg?s=192&g=1" className="card-img-top rounded-circle" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    
+  </div>
+  </div>
+    </>
+  )
+}
+
+export default Card
---
+++ frontend/src/components/Team/Team.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import  "./Team.css"
+import Card from './Card'
+function Team() {
+  return (
+    <div>
+        <div className='container border rounded ' >
+            <div className='name_container bg-info text-white bg-gradient border rounded'>
+            <h2 >Professor Incharge </h2>
+            <Card/>
+            </div>
+
+        </div>
+     
+        
+
+      
+    </div>
+  )
+}
+
+export default Team
