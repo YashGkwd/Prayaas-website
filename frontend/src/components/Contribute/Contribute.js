6985b4eb1260d0913c22ab85b783949749187da9
Yash Gaikwad <gaikwadyash412@gmail.com>
2023-03-26 01:06:16 +0530

    added contribute page, slider

--- frontend/src/components/Contribute/Contribute.js
+++ frontend/src/components/Contribute/Contribute.js
@@ -4,8 +4,24 @@ function Contribute() {
   
   return (
     <>
-      <div className='container1'>
-      <button type="button" className="btn btn-primary btn-lg fw-bold">Contribute</button>
+      <div className='contribute-cont'>
+        <div className='contribute-text'>
+        <h2  className='title-contribute'>Contribute now </h2>
+        <p className='para-contribute'>your one small help can bring a big change in the society ansdfelrgerg erg erg edrthg er etyg eth eth ....</p>
+        <div className="row g-2 align-items-center">
+      <div className="col-auto">
+        <label for="inputAmount" className="amount">Amount</label>
+      </div>
+      <div className="col-auto">
+        <input type="number"  className="form-control" />
+      </div>
+      </div>
+  
+      <button className="btn btn-primary btn-lg " type="submit">Contribute </button>
+        
+        </div>
+
+      
       </div>
     </>
   )
