679d02173a3b6df011da7b0954e3370835a8ca4d
Yash Gaikwad <gaikwadyash412@gmail.com>
2023-04-12 00:04:05 +0530

    updated

--- backend/Server.js
+++ backend/Server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose")
 const cors = require("cors");
-const PORT = process.env.port || 5000;
+const path = require("path")
+const PORT =  5000 || process.env.port;
 require("./Models/registerSchema")
 const REGISTER = mongoose.model("REGISTER");
 
@@ -17,4 +18,13 @@ app.use(express.json());
 app.use(cors())
 app.use(require("./router/router"))
 
+// Serving the frontend
+app.use(express.static(path.join(__dirname,'./frontend/build')));
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./frontend/build/index.html"),
+    function(err){
+        res.status(500).send(err);
+    }
+    )
+})
 app.listen(PORT);
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["frontend/src/components/Home/Home.css", "frontend/src/index.css"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
